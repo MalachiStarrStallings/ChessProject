4f7835651d4a944f189aecfe45b10b63051283ff
Malachi Starr Stallings <mstarrstallings99@unm.edu>
2025-04-29 03:16:51 -0600

    Added documentation to board, also added basic functionality to remove a piece, also overloaded getPieceAt method to work with algebraic notation, and also fixed not updating the pieces location itself, not just where it is in the board.

--- ChessConsoleVer/src/brains/Board.java
+++ ChessConsoleVer/src/brains/Board.java
@@ -1,7 +1,11 @@
 package brains;
 import pieces.*;
 import java.awt.*;
-
+/*
+This class is the "physical" game board where the pieces are stored, added, moved, and removed.
+The board is "dumb" and doesn't know if a given move is actually following the rules.
+All the board needs to do is store, track, move, and remove pieces during gameplay.
+ */
 public class Board {
     // 8x8 grid of piece objects used to represent the chess board
     private Piece[][] chessBoard;
@@ -10,14 +14,31 @@ public class Board {
         this.chessBoard = new Piece[8][8]; // Initialize the game board
 
     }
-
+    // Method used only on game startup, all other times you move a piece, well... you are moving it
     public void placePiece(Piece piece) {
         Point location = piece.getCordPosition();
         chessBoard[location.y][location.x] = piece;
     }
-
+    // Method used to retrive a piece at a specific coordinate
     public Piece getPieceAt(int i, int j) {
         return chessBoard[j][i];
+    }
+    // Overloaded method for retrieval in algebraic notation
+    public Piece getPieceAt(String position) {
+        Point point = Move.decodePosition(position);
+        return chessBoard[point.y][point.x];
+    }
+
+    public Piece removePieceAt(int i, int j) {
+        Piece removed = chessBoard[j][i];
+        chessBoard[j][i] = null;
+        return removed;
+    }
+    public Piece removePieceAt(String position) {
+        Point point = Move.decodePosition(position);
+        Piece removed = chessBoard[point.y][point.x];
+        chessBoard[point.y][point.x] = null;
+        return removed;
     }
 
     public void movePiece(Move move) {
@@ -31,9 +52,15 @@ public class Board {
         if ((startLoc.x == endLoc.x) && (startLoc.y == endLoc.y)) {
             throw new IllegalArgumentException("Illegal move! Start cell and end cell must be different");
         }
+        // Update the piece location in the board, and set the old location as null
         Piece chosenPiece = chessBoard[startLoc.y][startLoc.x];
         chessBoard[startLoc.y][startLoc.x] = null;
         chessBoard[endLoc.y][endLoc.x] = chosenPiece;
+
+        // Update the piece location inside the piece itself, and mark it as having moved
+        chosenPiece.setHasMoved(true);
+        chosenPiece.setAlgPosition(move.getTargetPosition());
+        chosenPiece.setCordPosition(endLoc);
     }
 
 
